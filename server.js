dba9c4398df08e5e7f1f05810cbe98da8b07c3dd
Solar Tech <149142801+solarapptech@users.noreply.github.com>
2024-11-20 12:02:31 -0400

    Update server.js

--- server.js
+++ server.js
@@ -15,22 +15,19 @@ const bnc_data = 10;
 const python_process = spawner('python', ['./apibnc.py', bnc_data]);
 
 python_process.stdout.on('data', (data) => {
-  
+
 tasabinance = data.toString();
-new_info();
-
-});
-
-function new_info(){
+  
 app.get ('/info5', (req, res) =>{
-   res.setHeader('Content-Type', 'text/event-stream')
-   res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Content-Type', 'text/event-stream')
+    res.setHeader('Access-Control-Allow-Origin', '*')
 
-  const sendData = `data: ${JSON.stringify(tasabinance) +' Bs.'}\n\n`;
-  res.write(sendData);
+    const sendData5 = `data: ${JSON.stringify(tasabinance) +' Bs.'}\n\n`;
+    res.write(sendData5);
 
 })
-}
+
+});
 
   
 
