436d3c7a563c126b76ca800525a18c80da15db8e
Solar Tech <149142801+solarapptech@users.noreply.github.com>
2024-11-20 00:13:43 -0400

    Update server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ python_process.stdout.on('data', (data) => {
 
 });
 
-pythonProcess.stdout.on('close', (data) => { 
+python_process.stdout.on('end', (data) => { 
   
       app.get ('/info5', (req, res) =>{
       res.setHeader('Content-Type', 'text/event-stream')
