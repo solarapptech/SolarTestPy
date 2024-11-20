f9e695c614f85516e9eacc9dfa919cee4c10c250
Solar Tech <149142801+solarapptech@users.noreply.github.com>
2024-11-20 11:29:26 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
-var cors = require('cors')
-const express = require ('express')
-const app = express ()
-// const port = 8000
-let server = app.listen(8000);
-server.keepAliveTimeout = 80000;
-server.headersTimeout = 81000;
-app.use(cors())
-
 const spawner = require('child_process').spawn;
 
 const bnc_data = 10;
-
 const python_process = spawner('python', ['./apibnc.py', bnc_data]);
 
 python_process.stdout.on('data', (data) => {
-
   app.get ('/info5', (req, res) =>{
       res.setHeader('Content-Type', 'text/event-stream')
       res.setHeader('Access-Control-Allow-Origin', '*')
@@ -24,6 +13,5 @@ python_process.stdout.on('data', (data) => {
       const sendData5 = `data: ${JSON.stringify(tasabinance) +' Bs.'}\n\n`;
       res.write(sendData5);
   })
-
 });
 
